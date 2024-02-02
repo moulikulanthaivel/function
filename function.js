e299f8427b2e275d79463976b3a3a1e863919c49
moulikulanthaivel <moulikulanthaivel@gmail.com>
2024-02-01 23:59:47 -0800

    function

---
+++ function.js
@@ -0,0 +1,91 @@
+function wish() {
+  console.log("GOOD MORNING");
+}
+wish();
+wish();
+wish();
+wish();
+wish();
+wish();
+wish();
+
+
+
+
+// cls
+
+
+//3
+
+function add(a,b){
+
+console.log(a+b)
+
+}
+add(100,300000,33)
+
+
+//4
+
+
+function add(a,b,){
+
+  console.log(a+b)
+  
+  }
+  add(100,300000,20000000)
+  
+  
+//function name without parameter
+
+function add(){
+
+  console.log('addd')
+  
+  }
+  add()
+  
+  //5
+
+  function member(rahul,ram,ravi){
+
+    console.log(rahul + ram + ravi)
+  }
+
+  member(21 ,23 ,25 )
+
+
+  function add(a,b){
+
+    return a-b;
+    
+  }
+  let a1 =add(200,100)
+  console.log(a1)
+
+
+
+
+
+
+
+
+
+  function sub(rahul,ram){
+
+    return rahul + ram
+  }
+let t1 = add(22 ,7773)
+console.log(t1)
+
+
+
+//csl age
+
+function cal_Age(year){
+
+  return 2024-year;
+}
+
+let age = cal_Age(2001)
+console.log(age)
